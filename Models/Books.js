786d5a1075262a740e076234d5686c10f035b163
augustinebest <augustusbest00@gmail.com>
2018-07-05 16:06:02 +0100

    added view property for bookSchema

--- Models/Books.js
+++ Models/Books.js
@@ -3,5 +3,7 @@ const mongoose = require('mongoose');
 var bookSchema = mongoose.Schema({
     date: Date.now(),
     coverImg: String,
-    bookBody: String;
+    bookBody: String,
+    views: Number,
+    
 })
